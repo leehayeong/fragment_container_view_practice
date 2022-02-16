a18efe3f58eb716794ee901f745b93517752dbf3
leehayeong <lhaayyy@naver.com>
2022-02-16 16:42:25 +0900

    [3] activity code 에서 fragment replace

--- app/src/main/java/com/example/fragmentcontainerviewpractice/MainActivity.kt
+++ app/src/main/java/com/example/fragmentcontainerviewpractice/MainActivity.kt
@@ -11,5 +11,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setFragment()
+    }
+
+    private fun setFragment() {
+        val aFragment = AFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.apply {
+            replace(binding.fragmentContainer.id, aFragment)
+            addToBackStack(null)
+            commit()
+        }
     }
 }
